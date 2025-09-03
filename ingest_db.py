a84c79a2d50cfb90148bd8cd96d543b03d5d3ce3
Ravi Karan Anand <ravikaran90@yahoo.com>
2025-09-03 09:28:14 -0700

    Update ingest_db.py

--- ingest_db.py
+++ ingest_db.py
@@ -4,4 +4,23 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from uuid import uuid4
 
+from dotenv import load_dotenv
+load_dotenv()
 
+DATA_PATH=r"data"
+CHROMA_PATH=r"chroma_db"
+
+#Initiating the Embeddings Model
+embeddings_model=OpenAIEmbeddings(model="text-embedding-3-large")
+
+#Initiate the Vector Store
+vector_store=Chroma(
+  collection_name="example_collection",
+  embedding_function=embeddings_model,
+  persist_directory=CHROMA_PATH,
+)
+
+#Loading the PDF Document
+loader=PyPDFDirectoryLoader(DATA_PATH)
+
+raw_documents=loader.load()
