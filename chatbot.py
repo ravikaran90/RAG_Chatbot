58354c3b65331ebf09fd97dfa822273fedfca61f
Ravi Karan Anand <ravikaran90@yahoo.com>
2025-09-03 09:54:40 -0700

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from Langchain_chroma import Chroma
+import gradio as gr
+
+#Import the env file
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
